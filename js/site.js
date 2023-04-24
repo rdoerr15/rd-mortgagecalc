450a832032daf18b505745d2cd22c90349c861e1
Ryan Doerr <ryan.doerr15@gmail.com>
2023-04-24 18:25:53 -0400

    edits

--- js/site.js
+++ js/site.js
@@ -1,104 +1,127 @@
-function totalMonthlyPayment() {
-    //get the inputs
-    let loanAmount = document.getElementById('loanAmount').value;
-    let termMonths = document.getElementById('termMonths').value;
-    let rate = document.getElementById('interestRate').value;
+//get values from form
+//validate inputs
+function getValues() {
+    let principal = document.getElementById('amount').value;
+    let term = document.getElementById('term').value;
+    let rate = document.getElementById('interest').value;
 
-    //turn them into integers
-    loanAmount = parseFloat(loanAmount);
-    termMonths = parseFloat(termMonths);
+    principal = parseFloat(principal);
+    term = parseInt(term);
     rate = parseFloat(rate);
 
-    //make calculation
-    let monthlyPayment = (loanAmount * (rate / 1200)) / (1 - ((1 + rate / 1200)**(-termMonths)));
+    if (!isNaN(principal) && !isNaN(term) && !isNaN(rate)
+        && principal > 0 && term > 0 && rate > 0) {
+          
+          let totals = calculateTotals(principal, term, rate);
+          let payments = calculatePayments(principal, term, rate);
 
-    //display result
-    let resultDiv = document.getElementById('monthlyPayment');
-    resultDiv.innerHTML = Math.round(monthlyPayment).toLocaleString();
+          displayTotals(totals);
+          displayPayments(payments);
+
+    } else {
+      Swal.fire({
+        icon: 'error',
+        backdrop: false,
+        title: 'Sorry!',
+        text: 'Please eneter valid numbers for your loan details.'
+      })
+    }
+}
+
+//calculate loan payments
+//calculate the totals
+function calculateTotals(principal, term, rate) {
+  //get monthly payment
+  let monthlyPayment = calculateMonthlyPayment(principal, term, rate)
+  //total cost = monthly payment * term
+  let totalCost = monthlyPayment * term;
+  //total int = cost - principal
+  let totalInterest = totalCost - principal;
+
+  let totals = {
+    monthlyPayment: monthlyPayment,
+    principal: principal,
+    cost: totalCost,
+    interest: totalInterest
+  };
+
+  return totals;
+}
+
+//calculate the amortization schedule
+function calculatePayments(principal, term, rate) {
+  //get monthly payment
+    let monthlyPayment = calculateMonthlyPayment(principal, term, rate)
+  
+  //calc remaining values
+    let balance = principal;
+    let totalInterest = 0;
+
+    let payments = [];
+
+    for (let month = 1; month <= term; month += 1) {
+        let interest = balance * (rate/1200);
+        let principalPayment = monthlyPayment - interest;
+        totalInterest = totalInterest + interest; //or totalInterest += interest
+        balance = balance - principalPayment;
+
+        let loanPayment = {
+          month: month,
+          payment: monthlyPayment,
+          principal: principalPayment,
+          interest: interest,
+          totalInterest: totalInterest,
+          balance: Math.abs(balance) 
+        }
+        payments.push(loanPayment);
+      }
+    return payments;
+  }
+
+function calculateMonthlyPayment(principal, term, rate) {
+    let monthlyRate = rate / 1200;
+    let monthlyPayment = (principal * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -term));
 
     return monthlyPayment;
 }
 
-function monthlyPaymentData() {
-    let loanAmount = document.getElementById('loanAmount').value;
-    let termMonths = document.getElementById('termMonths').value;
+//display totals on page
+//display the table rows
+function displayTotals(loanTotals) {
+    let formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
 
-    loanAmount = parseFloat(loanAmount);
-    termMonths = parseFloat(termMonths);
-
-    let totalPrincipal = loanAmount;
-
-    let monthlyPayment = totalMonthlyPayment();
-
-    let totalCost = monthlyPayment * termMonths;
-    
-    let totalInterest = totalCost - totalPrincipal;
-
-    //display results
-    let principalResult = document.getElementById('totalPrincipal');
-    principalResult.innerHTML = Math.round(totalPrincipal).toLocaleString();
-
-    let intResult = document.getElementById('totalInt');
-    intResult.innerHTML = Math.round(totalInterest).toLocaleString();
-
-    let costResult = document.getElementById('totalCost');
-    costResult.innerHTML = Math.round(totalCost).toLocaleString();
+    document.getElementById('monthlyPayment').textContent = formatter.format(loanTotals.monthlyPayment);
+    document.getElementById('totalPrincipal').textContent = formatter.format(loanTotals.principal);
+    document.getElementById('totalInterest').textContent = formatter.format(loanTotals.interest);
+    document.getElementById('totalCost').textContent = formatter.format(loanTotals.cost);
 }
 
-function generateMonth() {
-  let termMonths = document.getElementById('termMonths').value;
-  termMonths = parseFloat(termMonths);
-  
-  let start = 0;
-  let end = termMonths;
-  
-  let monthArray = [];
+function displayPayments(loanPayments) {
+    const tableRowTemplate = document.getElementById('paymentsRow');
+    let tableBody = document.getElementById('paymentsTable');
+    let formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
 
-  for (let month = start; month <= end; month++) {
-    monthArray.push(month); 
-  }
+    for(let i = 0; i < loanPayments.length; i++) {
+        let payment = loanPayments[i];
 
-  return monthArray; 
-}
+        let tableRow = document.importNode(tableRowTemplate.content, true);
+        let tds = tableRow.querySelectorAll('td');
 
-function calculateBalance() {
-    let loanAmount = document.getElementById("loanAmount").value;
-    loanAmount = parseFloat(loanAmount);
+        tds[0].textContent = payment.month;
+        tds[1].textContent = formatter.format(payment.payment);
+        tds[2].textContent = formatter.format(payment.principal);
+        tds[3].textContent = formatter.format(payment.interest);
+        tds[4].textContent = formatter.format(payment.totalInterest);
+        tds[5].textContent = formatter.format(payment.balance);
 
-
-    let monthArray = generateMonth();
-
-    for (let i = 0; i < monthArray.length; i++) {
-        let startingBalance = loanAmount[i];
-
-        currentBalance = 
+        tableBody.appendChild(tableRow);
     }
 }
 
-function calculateInt() {
-    let loanAmount = document.getElementById("loanAmount").value;
-    loanAmount = parseFloat(loanAmount);
-
-    let rate = document.getElementById("interestRate").value;
-    rate = parseFloat(rate);
-
-    let monthArray = generateMonth();
-
-    let int = 0;
-
-    for (let i = 0; i < monthArray.length; i++) {
-      currentInt = loanAmount * (rate / 1200)[i];
-    
-      int = int + currentInt;
-    }
-    return int;
-}
-
-function calculatePrincipalPayment() {
-    
-}
-
-function calculateTotalInt() {
-
-}
 
